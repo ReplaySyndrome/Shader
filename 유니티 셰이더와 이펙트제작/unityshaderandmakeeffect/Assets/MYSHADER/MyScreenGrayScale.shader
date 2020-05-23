5779d9cff6f2ecee9f7c6c9b5e10014308c3d0d6
kisum1024 <43131669+kisum1024@users.noreply.github.com>
2020-05-24 01:06:38 +0900

    외안되지?

---
+++ 유니티 셰이더와 이펙트제작/unityshaderandmakeeffect/Assets/MYSHADER/MyScreenGrayScale.shader
@@ -0,0 +1,39 @@
+﻿Shader "MyStudy/Chapter09/MyScreenGrayScale" {
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_Luminosity("Luminosity",Range(0.0,1)) = 1.0
+	}
+		SubShader
+		{
+			Pass
+			{
+					Tags { "RenderType" = "Opaque" }
+					LOD 200
+
+					CGPROGRAM
+				// Physically based Standard lighting model, and enable shadows on all light types
+				#pragma vertex vert_img
+				#pragma frgment frag
+				#pragma fragmentoption ARB_precision_hint_fastest
+				#include "UnityCG.cginc"
+
+				uniform sampler2D _MainTex;
+				fixed _Luninosity;
+
+				fixed frag(v2f_img i) : COLOR
+				{
+					fixed4 renderTex = tex2D(_Maintex,i.uv);
+					float luminosity 0.299 * renderTex.r + 0.587 * renderTex.g = 0.114 * renderTex.b;
+					fixed4 finalColor = lerp(renderTex, luminosity, _Luminosity);
+					renderTex.rgb = finalColor;
+					return renderTex;
+				}
+					ENDCG
+			}
+		
+		}
+			FallBack off
+}
+	
+
---
+++ 유니티 셰이더와 이펙트제작/unityshaderandmakeeffect/Assets/MyScript/MyTestRenderImage.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[ExecuteInEditMode]
+public class MyTestRenderImage : MonoBehaviour {
+	#region Variables
+	public Shader curShader;
+	public float grayscaleAmount = 1.0f;
+	private Material screenMat;
+    #endregion
+
+    #region Properites
+	Material ScreenMat
+	{
+		get
+		{
+			if (screenMat == null)
+			{
+				screenMat = new Material(curShader);
+				screenMat.hideFlags = HideFlags.HideAndDontSave;
+			}
+			return screenMat;
+		}
+	}
+    #endregion
+
+
+
+    // Use this for initialization
+    void Start () {
+		if(!SystemInfo.supportsImageEffects)
+		{
+			enabled = false;
+			return;
+		}
+
+		if(!curShader && !curShader.isSupported)
+		{
+			enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grayscaleAmount = Mathf.Clamp(grayscaleAmount, 0.0f, 1.0f);
+	}
+
+	void OnDisable()
+	{
+		if (screenMat)
+		{
+			DestroyImmediate(screenMat);
+		}
+	}
+
+	void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+	{
+		if(curShader != null)
+		{
+			//screenMat.SetFloat("_Luminosity", grayscaleAmount);
+			Graphics.Blit(sourceTexture, destTexture, screenMat);
+		}
+		else
+		{
+			Graphics.Blit(sourceTexture, destTexture);
+		}
+	}
+}
